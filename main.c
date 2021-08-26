3244caa36fd477db76a9c237846f0c9c77efe15e
Spencer Nicol <spencern319@gmail.com>
2021-08-26 07:39:36 -0500

    simple multi-thread example.

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+// Normal C function that is executed as a thread when its name is specified
+// in the pthread_create()
+void *threadFun(void *vargp) {
+    sleep(1);
+    printf("Printing from thread.\n");
+    return NULL;
+}
 
 int main() {
-    printf("Hello, World!\n");
+    pthread_t tid;
+    printf("Before Thread.\n");
+    pthread_create(&tid, NULL, threadFun, NULL);
+    pthread_join(tid, NULL);
+    printf("After Thread.\n");
     return 0;
 }
